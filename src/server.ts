0862116e93f374e44820d5d7c9af4f8635bc8307
SoloLineR <thesololiner@mail.ru>
2024-11-06 15:32:25 +0300

    fix linter errors and add express

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,12 @@
-function s() {
-  let a = "a";
-}
+import express from "express";
+import { Express, Request, Response } from "express";
+const server: Express = express();
+server.use(express.json());
+
+server.get("/api", (req: Request, res: Response) => {
+  res.send("aboba");
+});
+
+server.listen(3002, () => {
+  console.log(`server on port ${3002}`);
+});
