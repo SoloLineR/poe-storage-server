0c01b745fe9c0f1bf67cb4e9018539bdf665adbc
SoloLineR <thesololiner@mail.ru>
2024-11-08 22:24:05 +0300

    add league table

--- src/sql/sql.sql
+++ src/sql/sql.sql
@@ -30,6 +30,12 @@ CREATE TABLE  author (
     url VARCHAR NOT NULL
 );
 
+CREATE TABLE league (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL
+);
+
+
 CREATE TABLE build (
     id SERIAL PRIMARY KEY,
     name VARCHAR NOT NULL,
@@ -39,9 +45,13 @@ CREATE TABLE build (
     author_id INTEGER NOT NULL,
     poe_class_id INTEGER NOT NULL,
     ascendancy_class_id INTEGER NOT NULL,
+    league_id INTEGER NOT NULL,
     FOREIGN KEY (poe_class_id) REFERENCES poe_class(id),
     FOREIGN KEY (ascendancy_class_id) REFERENCES ascendancy_class(id), 
     FOREIGN KEY (build_type_id) REFERENCES build_type(id), 
     FOREIGN KEY (skill_id) REFERENCES skill(id),  
-    FOREIGN KEY (author_id) REFERENCES author(id)  
+    FOREIGN KEY (author_id) REFERENCES author(id),
+    FOREIGN KEY (league_id) REFERENCES league(id)
 );
+
+     
